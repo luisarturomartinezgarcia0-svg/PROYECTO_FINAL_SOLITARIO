4aa1687a6608cf638452390d30bcd09fd15b5b60
luisarturomartinezgarcia0-svg <luisarturomartinezgarcia0@gmail.com>
2026-04-24 13:59:32 +0000

    cuarto commit

---
+++ frontend/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+
+const conexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "02161800",
+    database: "jardin_de_daisiry"
+});
+
+conexion.connect((error) => {
+    if (error) {
+        console.log("Error de conexión:", error);
+        return;
+    }
+    console.log("Conexión exitosa a MySQL");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000");
+});
