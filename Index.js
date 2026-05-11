a81c258ef166b7a2eb312d2e2b20e7de9c16207b
Luis Arturo <luisarturomartinezgarcia0@gmail.com>
2026-05-10 20:12:50 -0400

    cambios terminados

--- Index.js
+++ Index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 
 const app = express();
 
-// SETTINGS
-app.set('port', process.env.PORT || 5500);
+// CONFIGURACIÓN
+app.set('port', process.env.PORT || 9000);
 
+// HANDLEBARS
 app.engine('.hbs', exphbs.engine({
     extname: '.hbs',
     defaultLayout: 'main',
@@ -16,25 +18,22 @@ app.engine('.hbs', exphbs.engine({
 }));
 
 app.set('view engine', '.hbs');
+
 app.set('views', path.join(__dirname, 'src/views'));
 
 // MIDDLEWARES
-app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// STATIC FILES
+// ARCHIVOS ESTÁTICOS
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ROUTES
-app.use(require('./src/routes/indexRoutes'));
-
-// ERROR 404
-app.use((req, res) => {
-    res.status(404).send('Página no encontrada');
+// RUTA PRINCIPAL
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-// SERVER
+// SERVIDOR
 app.listen(app.get('port'), () => {
-    console.log('Servidor en puerto', app.get('port'));
+    console.log(`Servidor corriendo en puerto ${app.get('port')}`);
 });
--- src/config/db.js
+++ src/config/db.js
@@ -2,17 +2,20 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.localhost,
-    user: process.env.root,
-    database: process.env.jardin_de_daisiry
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
+
     if (err) {
         console.log(err);
     } else {
         console.log('MySQL conectado');
     }
+
 });
 
 module.exports = connection;
